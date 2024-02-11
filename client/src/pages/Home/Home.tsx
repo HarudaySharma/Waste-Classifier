0393bd731ff6db924ced80806b2dbb1cb13f10ae
Harshit-Thakur21 <harshitthakur2102@gmail.com>
2024-02-11 14:09:12 +0530

    added home page ui(not completed)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,40 @@
 import styles from  "./Home.module.scss"
 import Header from "../../components/ui/Header/Header";
+import icon1 from '../../assets/vector.png'
+import icon2 from '../../assets/upload.png'
+import border from '../../assets/rectangle-2-stroke.png'
+import rightImage from '../../assets/homeImage.jpg'
+
 
 function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <Header/>
+            <div className={styles.bodyContent}>
+                <div className={styles.leftContent}>
+                    <h1 className={styles.heading1}>WASTE</h1>
+                    <h1 className={styles.heading2}>CLASSIFIER</h1>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.iconButton}>
+                            <img src={icon1} alt="scanIcon" className={styles.icon}/>
+                            SCAN
+                        </button>
+                        <button className={styles.iconButton}>
+                            <img src={icon2} alt="uploadIcon" className={styles.icon}/>
+                            UPLOAD
+                        </button>
+                    </div>
+                </div>
+
+                <div className={styles.square}>
+                    <div className={styles.border}>
+                        <img src={border} alt="border"/>
+                    </div>
+
+                    <img src={rightImage} alt="homeImage" className={styles.bigImage}/>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 export default Home;
