b1634b2e465f362e80270e9339b6295cd64541a8
HarudaySharma <harudayvasist@gmail.com>
2024-03-08 17:17:13 +0530

    partial: added the hamburger menu

--- client/src/components/ui/Button/Button.tsx
+++ client/src/components/ui/Button/Button.tsx
@@ -2,15 +2,26 @@ import React, { FC } from 'react'
 
 interface ButtonProps {
     className?: string;
+    id?: string;
     onClickHandler?: React.MouseEventHandler<HTMLButtonElement>
     size?: string;
     variant?: string
+    aria_expanded?: boolean;
+    aria_labelledby?: string;
+    aria_label?: string;
     children?: React.ReactNode;
 }
 
-const Button: FC<ButtonProps> = ({ className, children , onClickHandler}) => {
+const Button: FC<ButtonProps> = ({ className, id, children, onClickHandler , aria_expanded, aria_labelledby, aria_label}) => {
     return (
-        <button className={className} onClick={onClickHandler}>
+        <button
+            id={id}
+            className={className}
+            aria-expanded={aria_expanded}
+            aria-labelledby={aria_labelledby}
+            aria-label={aria_label}
+            onClick={onClickHandler}
+        >
             {children}
         </button>
     )
--- client/src/components/ui/Header/Header.tsx
+++ client/src/components/ui/Header/Header.tsx
@@ -1,62 +1,111 @@
 import logo from '../../../assets/logo.png'
 import icon from '../../../assets/address-book.png'
+import openIcon from "../../../assets/hamburger.svg"
+import closeIcon from "../../../assets/close.svg"
 import Button from "../Button/Button";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
+
+
+type CallbackFunction = () => void;
+let openMobile: CallbackFunction | null = null;
+let closeMobile: CallbackFunction | null;
 
 const Header = () => {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const loadMobileScript = async () => {
+            const { openMobileMenu, closeMobileMenu } = await import('./mobileScript.js');
+            openMobile = openMobileMenu;
+            closeMobile = closeMobileMenu;
+        };
+
+        loadMobileScript();
+    }, []);
+
+    const navOpenHandler = () => {
+        if (openMobile)
+            openMobile();
+    }
+
+    const onCloseHandle = () => {
+        if (closeMobile)
+            closeMobile();
+    }
+
     return (
         <nav className='nav'>
             <div className={`nav__logo`}>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" />
             </div>
 
-            <div className='nav__buttons'>
+            <Button
+                id='nav-open'
+                className="nav__open"
+                aria_expanded={false}
+                aria_labelledby="nav-label"
+                onClickHandler={navOpenHandler}
+            >
+                <img src={openIcon} alt="" width="40" height="24" />
+            </Button>
+            <div className='nav__menu' role='dialog' aria-labelledby='nav-label'>
                 <Button
-                    className='nav__buttons__button'
-                    onClickHandler={(e) => {
-                        e.preventDefault();
-                        navigate('/');
-                    }}
+                    id='nav-close'
+                    className="nav__close"
+                    aria_label="Close"
+                    onClickHandler={onCloseHandle}
                 >
-                    <span>Home</span>
+                    <img src={closeIcon} alt="" width="28" height="27" />
                 </Button>
-                <Button
-                    className='nav__buttons__button'
-                    onClickHandler={(e) => {
-                        e.preventDefault();
-                        navigate('/info');
-                    }}
-                >
-                    <span>Info</span>
-                </Button>
-                <Button
-                    className='nav__buttons__button'
-                    onClickHandler={(e) => {
-                        e.preventDefault();
-                        navigate('/goals');
-                    }}
-                >
-                    <span>Goals</span>
-                </Button>
-                <Button
-                    className='nav__buttons__button'
-                    onClickHandler={(e) => {
-                        e.preventDefault();
-                        navigate('/about');
-                    }}
-                >
-                    <span>About Us</span>
-                </Button>
-                <Button
-                    className='nav__buttons__button'
-                    onClickHandler={(e) => {
-                        e.preventDefault();
-                        navigate('/contact');
-                    }}
-                >
-                    <img src={icon} alt="Contact Icon" className='nav__buttons__button__icon' />
-                </Button>
+
+                <div className='nav__menu__buttons'>
+                    <Button
+                        className='nav__menu__buttons__button'
+                        onClickHandler={(e) => {
+                            e.preventDefault();
+                            navigate('/');
+                        }}
+                    >
+                        <span>Home</span>
+                    </Button>
+                    <Button
+                        className='nav__menu__buttons__button'
+                        onClickHandler={(e) => {
+                            e.preventDefault();
+                            navigate('/info');
+                        }}
+                    >
+                        <span>Info</span>
+                    </Button>
+                    <Button
+                        className='nav__menu__buttons__button'
+                        onClickHandler={(e) => {
+                            e.preventDefault();
+                            navigate('/goals');
+                        }}
+                    >
+                        <span>Goals</span>
+                    </Button>
+                    <Button
+                        className='nav__menu__buttons__button'
+                        onClickHandler={(e) => {
+                            e.preventDefault();
+                            navigate('/about');
+                        }}
+                    >
+                        <span>About Us</span>
+                    </Button>
+                    <Button
+                        className='nav__menu__buttons__button'
+                        onClickHandler={(e) => {
+                            e.preventDefault();
+                            navigate('/contact');
+                        }}
+                    >
+                        <img src={icon} alt="Contact Icon" className='nav__menu__buttons__button__icon' />
+                    </Button>
+                </div>
             </div>
 
         </nav>
