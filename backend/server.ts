b010d23f43907cd20f48e22aad137774db21df62
HarudaySharma <harudayvasist@gmail.com>
2024-03-11 12:13:23 +0530

    production changes

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,8 @@
 import express, { Application } from 'express';
-import cors from "cors"
 import {config} from 'dotenv';
 import initialRoute from "./routes/initial.route.js"
 import { loadModel } from './utils/lgModel.js';
+import path from 'path'
 
 config();
 
@@ -11,12 +11,19 @@ const app: Application = express();
 
 const PORT: string = process.env.PORT || "3000";
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+
 app.listen(PORT, () => { 
     console.log(`server listening on PORT:${PORT}`);
     loadModel();
 })
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/api/' , initialRoute);
